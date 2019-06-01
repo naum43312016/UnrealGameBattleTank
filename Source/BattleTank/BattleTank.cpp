02fc35725d2a059de984bea6df2b35ce47e0c22a
naum <naum43312016@gmail.com>
2019-06-01 17:11:48 +0300

    test2

--- Source/BattleTank/BattleTank.cpp
+++ Source/BattleTank/BattleTank.cpp
@@ -3,4 +3,4 @@
 #include "BattleTank.h"
 #include "Modules/ModuleManager.h"
 
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BattleTank, "BattleTank" );//-----
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BattleTank, "BattleTank" );
